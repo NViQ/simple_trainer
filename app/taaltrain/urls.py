7a02d16dba687e464d1f42a9288d0e36ede037ae
Павел <naveq4358@gmail.com>
2024-01-22 19:56:02 +0300

    some changes

--- app/taaltrain/urls.py
+++ app/taaltrain/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app.api.v1.text_chat import UserQueryView
 
 urlpatterns = [
-    path('process-query/', UserQueryView.as_view(), name='process_query'),
+    path('check-writing/', UserQueryView.as_view(), name='check-writing'),
 ]
