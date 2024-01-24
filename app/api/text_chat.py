3f6dad796ccad806aad46731261f9821072adacc
Павел <naveq4358@gmail.com>
2024-01-24 19:40:27 +0300

    some changes

--- app/api/text_chat.py
+++ app/api/text_chat.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
@@ -13,14 +13,6 @@ load_dotenv()
 OPENAI_API_KEY = os.environ.get('OPENAI_KEY')
 
 
-# def format_system_prompt(user_query):
-#     return (
-#         "Проверь этот текст на наличие грамматических или пунктуационных ошибок, "
-#         "верни процент верного текста и если есть слова в которых были ошибки: "
-#         "\n\n" + user_query
-#     )
-
-
 class UserQueryView(APIView):
     def post(self, request, format=None):
         user_id = request.data.get('user_id')
@@ -29,19 +21,17 @@ class UserQueryView(APIView):
         if user_query is None:
             return Response({'error': 'Error there is no request text'}, status=status.HTTP_400_BAD_REQUEST)
 
-        prompt = ChatPromptTemplate.from_template(
+        # Шаблон запроса, использующий переменную
+        prompt_template = (
             "Check this text for grammatical errors, the words in which there were errors, "
-            "if any, and the percentage of the correct text:" + user_query
-            )
-        output_parser = StrOutputParser()
-        llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_API_KEY)
-        chain = (
-                {"user_query": RunnablePassthrough()}
-                | prompt
-                | llm
-                | output_parser
+            "if any, and the percentage of the correct text: {user_query}"
         )
+        prompt = ChatPromptTemplate.from_template(prompt_template)
+        model = ChatOpenAI(model="gpt-3.5-turbo-1106", openai_api_key=OPENAI_API_KEY)
+        output_parser = StrOutputParser()
+        chain = prompt | model | output_parser
 
-        response = chain.invoke("user_query")
+        # Передача user_query как словаря
+        response = chain.invoke({"user_query": user_query})
 
         return Response({'user_id': user_id, 'response': response})
